f05c91746be7d12c23594e9f40e32746fc656aeb
SYangDong <shouyang.dong@gmail.com>
2022-10-13 17:22:10 +0800

    add cache manager

--- test/tools/python_format.sh
+++ test/tools/python_format.sh
@@ -2,6 +2,6 @@
 black  parser/
 black  test/
 pyupgrade --py38-plus parser/*.py
-pyupgrade --py38-plus test/*.py
+pyupgrade --py38-plus test/python/*.py
 pydocstringformatter --style=pep257 --write --no-split-summary-body --capitalize-first-letter parser/*.py
-pydocstringformatter --style=pep257 --write --no-split-summary-body --capitalize-first-letter test/*.py
+pydocstringformatter --style=pep257 --write --no-split-summary-body --capitalize-first-letter test/python/*.py
