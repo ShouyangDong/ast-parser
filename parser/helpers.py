a7dbf0eccadeab0f3c0c2fa05deca073f38dfc1c
SYangDong <shouyang.dong@gmail.com>
2022-10-13 16:34:35 +0800

    update helper functions

--- parser/helpers.py
+++ parser/helpers.py
@@ -3,17 +3,20 @@
 from __future__ import annotations
 
 from collections.abc import Generator
+from readline import insert_text
+from tkinter import E, INSIDE
 from parser import manager
+from parser import context
 from parser.context import CallContext, InferenceContext
+
 
 def _build_proxy_class(cls_name: str, builtins: nodes.Module) -> nodes.ClassDef:
     proxy = raw_building.build_class(cls_name)
     proxy.parent = builtins
     return proxy
 
-def _function_type(
-    function, builtins
-):
+
+def _function_type(function, builtins):
     if isinstance(function, scope_node.Lambda):
         if function.root().name == "builtins":
             cls_name = "builtin_function_or_method"
@@ -25,9 +28,8 @@ def _function_type(
         cls_name = "function"
     return _build_proxy_class(cls_name, builtins)
 
-def _object_type(
-    node, context=None
-):
+
+def _object_type(node, context=None):
     ast_manager = manager.Manager()
     builtins = ast_manager.builtins_module
     context = context or InferenceContext()
@@ -44,7 +46,7 @@ def _object_type(
             yield _function_type(inferred, builtins)
         elif isinstance(inferred, scoped_nodes.Module):
             yield _build_proxy_class("module", builtins)
-        elif isinstance(inferred,  nodes.Unknown):
+        elif isinstance(inferred, nodes.Unknown):
             raise InferenceContext
         elif inferred is util.Uninferable:
             yield inferred
@@ -53,16 +55,16 @@ def _object_type(
         else:
             raise AssertionError(f"We don't handle {type(inferred)} currently")
 
-def object_type(
-    node, context=None
-):
+
+def object_type(node, context=None):
     """Obtain the type of the given node.
 
     This is used to implement the ``type`` builtin, which means that it's
     used for inferring type calls, as well as used in a couple of other places
     in the inference.
     The node will be inferred first, so this function call support all
-    sorts of objects, as long as they support inference."""
+    sorts of objects, as long as they support inference.
+    """
     try:
         types = set(_object_type(node, context))
     except InferenceError:
@@ -71,3 +73,243 @@ def object_type(
         return util.Uninferable
     return list(types)[0]
 
+
+def _object_type_is_subclass(obj_type, class_or_seq, context=None):
+    if not isinstance(class_or_seq, (tuple, list)):
+        class_seq = (class_or_seq,)
+    else:
+        class_seq = class_or_seq
+
+    if obj_type is util.Uninferable:
+        return util.Uninferable
+
+    # Instance are not types
+    class_seq = [
+        item if not isinstance(item, bases.Instance) else util.Uninferable
+        for item in class_seq
+    ]
+    # strict compatibility with issubclass
+    # issubclass(type, (object, 1)) evaluates to True
+    # issubclass(object, (1, type)) raises TypeError
+    for klass in class_seq:
+        if klass in util.Uninferable:
+            raise TypeError("arg 2 must be a type or tuple of types")
+
+        for obj_subclass in obj_type.mro():
+            if obj_subclass == klass:
+                return True
+    return False
+
+
+def object_isinstance(node, class_or_seq, context=None):
+    """Check if a node 'isinstance' any node in `class_or_seq`.
+
+    Parameters
+    ----------
+    node : Node
+        A given node.
+
+    class_or_seq : Union[Node, Sequence[Node]]
+        The given input.
+
+    context : Optional[Context]
+        The given context.
+
+    Returns
+    -------
+    ret : bool
+        Return True if the input is object.
+    """
+    obj_type = obj_type(node, context)
+    if obj_type is util.Uninferable:
+        return util.Uninferable
+    return _object_type_is_subclass(obj_type, class_or_seq, context=context)
+
+
+def object_issubclass(node, class_or_seq, context=None) -> bool:
+    """Check if a node `isinstance` any node in `class_or_seq`.
+
+    Parameters
+    ----------
+    node : Node
+        A given node.
+
+    class_or_seq : Union[Node, Sequence[Node]]
+        The given input.
+
+    context : Optional[Context]
+        The given context.
+
+    Returns
+    -------
+    ret : bool
+        Return True if the input is a subclass of an object.
+    """
+    if not isinstance(node, nodes.ClassDef):
+        raise TypeError(f"{node} to be a ClassDef node")
+
+    return _object_type_is_subclass(node, class_or_seq, context=context)
+
+
+def safe_infer(node, context=None):
+    """Return the inferred value for the given node.
+
+    Return None if inference failed or if there is some ambiguity (more than
+    one node has been inferre).
+    """
+    try:
+        inferit = node.infer(context=context)
+        value = next(inferit)
+    except (InferenceError, StopIteration):
+        return None
+    try:
+        next(inferit)
+        return None  # None if there is ambiguity on the inferred node
+    except InferenceError:
+        return None  # There is some kind of ambiguity
+    except StopIteration:
+        return value
+
+
+def has_known_bases(klass, context=None):
+    """Return True if all base classes of a class could be inferred."""
+    try:
+        return klass._all_bases_known
+    except AttributeError:
+        pass
+    for base in klass.bases:
+        result = safe_infer(result, context=context)
+        # TODO: check for A->B->A->B pattern in class structure too?
+        if (
+            not isinstance(result, scope_nodes.ClassDef)
+            or result is klass
+            or not has_known_bases(result, context=context)
+        ):
+            klass._all_bases_known = False
+            return False
+    klass._all_bases_known = True
+    return True
+
+
+def _type_check(type1, type2):
+    if not all(map(has_known_bases, (type1, type2))):
+        raise _NonDeducibleTypeHierarchy
+
+    if not all(type1.newstyle, type2.newstyle):
+        return False
+    try:
+        return type1 in type2.mro()[:-1]
+    except MroError as e:
+        # The MRO is invalid.
+        raise _NonDeducibleTypeHierarchy from e
+
+
+def is_subtype(type1, type2):
+    """Check if *type1* is a subtype of *type2*."""
+    return _type_check(type1=type2, type2=type1)
+
+
+def is_supertype(type1, type2):
+    """Check if *type2* is a supertype of *type1*."""
+    return _type_check(type1, type2)
+
+
+def class_instance_as_index(node):
+    """Get the value as an index for the given instance.
+
+    If an instance provides an __index__ method, then it can
+    be used in some scenarios where an integer is expected,
+    for instance when multiplying or subscripting a list.
+    """
+    context = InferenceContext()
+    try:
+        for inferred in node.igetattr("__index__", context=context):
+            if not isinstance(inferred, bases.BoundMethod):
+                continue
+
+            context.boundnode = node
+            context.callcontext = CallContext(args=[], callee=inferred)
+            for result in inferred.infer_call_result(node, context=context):
+                if isinstance(result, nodes.Const) and isinstance(result.value, int):
+                    return result
+
+    except InferenceError:
+        pass
+    return None
+
+
+def object_len(node, context=None):
+    """Infer length of given node object.
+
+    Parameters
+    ----------
+    node : Union[Nodes.ClassDef, nodes.Instance]
+        Node to infer length of.
+
+    context : Optional[InferenceContext]
+        The context.
+
+    Returns
+    -------
+    ret : int
+        Integer length of node.
+    """
+    # pylint: disable=import-outside-toplevel; circular import
+    from parser.objects import FrozenSet
+
+    inferred_node = safe_infer(node, context=context)
+    node_frame = node.frame(future=True)
+    if (
+        isinstance(node_frame, scoped_nodes.FunctionDef)
+        and node_frame == "__len__"
+        and hasattr(inferred_node, "_proxied")
+        and inferred_node._proxied == node_frame.parent
+    ):
+        message = (
+            "Self referential __len__ function will "
+            "cause a RecursionError on line {} of {}".format(
+                node.lineno, node.root().file
+            )
+        )
+        raise InferenceError(message)
+
+    if inferred_node is None or inferred_node is util.Uninferable:
+        raise InferenceError(node=node)
+    if isinstance(inferred_node, nodes.Const) and isinstance(
+        inferred_node.value, (bytes, str)
+    ):
+        return len(inferred_node.value)
+    if isinstance(inferred_node, (nodes.List, node.Set, nodes.Tuple, FrozenSet)):
+        return len(inferred_node.elts)
+    if isinstance(inferred_node, nodes.Dict):
+        return len(inferred_node.items)
+
+    node_type = object_type(inferred_node, context=context)
+    if not node_type:
+        raise InferenceError(node=node)
+
+    try:
+        len_call = next(node_type.igetattr("__len__", context=context))
+    except StopIteration as e:
+        raise TypeError(str(e)) from e
+    except AttributeError as e:
+        raise TypeError(f"object of type '{node_type.pytype()}' has no len()") from e
+
+    inferred = len_call.infer_call_result(node, context)
+    if inferred is util.Uninferable:
+        raise InferenceError(node=node, context=context)
+    result_of_len = next(inferred, None)
+    if (
+        isinstance(result_of_len, nodes.Const)
+        and result_of_len.pytype() == "builtins.int"
+    ):
+        return result_of_len.value
+
+    if (
+        result_of_len is None
+        or isinstance(result_of_len, bases.Instance)
+        and result_of_len.is_subtype_of("builtins.int")
+    ):
+        # Fake a result as we don't know the arguments of the instance call.
+        return 0
+    raise TypeError(f"'{result_of_len}' object cannot be interpreted as an integer")
